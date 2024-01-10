cb761be99a1ddb1aaded06592fb9b3d5180718b8
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2024-01-10 03:37:55 -0800

    Test for 6 and 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -4,7 +4,7 @@
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
  */
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
 		return;
