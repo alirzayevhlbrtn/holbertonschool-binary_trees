c9052a6d3ff87ee52a534c3839feb2261985c8ec
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2024-01-10 03:25:39 -0800

    Test for 4 fix

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,5 +1,8 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_is_leaf - checks if node is leaf or not
+ * @node: pointer to node for check
+ * Return: 1 or 0
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
