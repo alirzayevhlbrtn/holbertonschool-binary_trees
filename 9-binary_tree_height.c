0767b603de45a6adbb3f578430a0261949174d99
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-01-10 14:52:14 -0800

    task 9 bug fiex

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,11 +9,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_h = 0, right_h = 0;
 
-	if (tree != NULL)
-	{
-		left_h = binary_tree_height(tree->left);
-		right_h = binary_tree_height(tree->right);
-	}
+	if (!tree || (!tree->right && !tree->left))
+		return (0);
+
+	left_h = binary_tree_height(tree->left);
+	right_h = binary_tree_height(tree->right);
+
 
 	return (1 + (left_h >= right_h ? left_h : right_h));
 }
