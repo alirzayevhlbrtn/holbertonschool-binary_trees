e3c5bb1135b785567774c855f2397cf31d20e552
alirzayevhlbrtn <elirzayevuzeyir1@gamil.com>
2024-01-10 00:51:20 -0800

    Test for 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+/**
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
+
+	if (newnode == NULL || parent == NULL)
+		return (NULL);
+
+	newnode->parent = parent;
+	newnode->n = value;
+	newnode->left = NULL;
+	newnode->right = NULL;
+
+	if (parent->left)
+	{
+		newnode->left = parent->left;
+		parent->left->parent = newnode;
+	}
+
+	parent->left = newnode;
+	return (newnode);
+}
