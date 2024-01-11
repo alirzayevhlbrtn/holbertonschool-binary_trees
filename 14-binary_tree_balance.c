51f96a13d6949de43c50586b2d773261b6b77a97
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-01-11 04:57:18 -0800

    Bug fixed

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,13 +9,7 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height, right_height;
-
 	if (tree == NULL)
 		return (0);
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	return (left_height - right_height);
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
