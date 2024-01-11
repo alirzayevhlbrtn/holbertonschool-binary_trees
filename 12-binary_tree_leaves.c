ef24090f037aff860aab3f065688e41f118c0a23
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-01-10 16:18:27 -0800

    bug fixed binary tree function

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,15 +8,15 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    	if (!tree)
-        	return (0);
+	if (!tree)
+		return (0);
 
-    	if (!tree->left && !tree->right)
+	if (!tree->left && !tree->right)
 		return (1);
 
-    	size_t left_leaves = binary_tree_leaves(tree->left);
-    	size_t right_leaves = binary_tree_leaves(tree->right);
+	size_t left_leaves = binary_tree_leaves(tree->left);
+	size_t right_leaves = binary_tree_leaves(tree->right);
 
-    	return left_leaves + right_leaves;
+	return (left_leaves + right_leaves);
 }
 
